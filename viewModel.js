29daf82992e2a910092559c0fba20335f5d5dd41
knrdk <konrad1991@gmail.com>
2015-12-30 23:33:14 +0100

    wyświetlanie wielu filmów

--- viewModel.js
+++ viewModel.js
@@ -1,29 +1,44 @@
 function User(name) {
     var self = this;
-    
+
     self.name = name;
     self.movies = ko.observableArray();
     self.isSelected = ko.observable(true);
     self.isLoaded = ko.observable(false);
-    
-    self.showSuccess = ko.computed(function() {
+
+    self.showSuccess = ko.computed(function () {
         return self.isLoaded() && self.isSelected();
     });
-    
-    self.showDanger = ko.computed(function() {
+
+    self.showDanger = ko.computed(function () {
         return self.isLoaded() && !self.isSelected();
     });
-    
-    self.showWarning = ko.computed(function() {
+
+    self.showWarning = ko.computed(function () {
         return !self.isLoaded();
     });
 }
 
 var CommonMoviesViewModel = function () {
     var self = this;
+
     this.users = ko.observableArray();
     this.newUserName = ko.observable("knrdk");
     this.currentUser = ko.observable();
+
+    self.commonMovies = ko.computed(function () {
+        var movies = [];
+
+        ko.utils.arrayForEach(self.users(), function (user) {
+            if (user.isSelected()) {
+                ko.utils.arrayForEach(user.movies(), function (movie) {
+                    movies.push(movie);
+                });
+            }
+        });
+
+        return movies;
+    });
 
     this.registerClick = function () {
         var newUser = new User(this.newUserName());
@@ -32,17 +47,17 @@ var CommonMoviesViewModel = function () {
         this.users.push(newUser);
         getMovies(newUser.name, this.callback);
     };
-    
-    this.selectUser = function(user){
-      var isSelected = user.isSelected();
-      user.isSelected(!isSelected);
-      self.currentUser(user);  
+
+    this.selectUser = function (user) {
+        var isSelected = user.isSelected();
+        user.isSelected(!isSelected);
+        self.currentUser(user);
     };
 
     this.getUserForName = function (userName) {
         var output = null;
         ko.utils.arrayForEach(this.users(), function (user) {
-            if(user.name == userName){
+            if (user.name == userName) {
                 output = user;
             }
         });
